74df6b57226d03e5228c0c190011c8e6d1ea672d
poon <poonyapat.y@ku.th>
2017-12-17 20:28:21 +0700

    remove test system (for commandline) edit main for javafx

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import GUI_Javafx.NoteGUISystem;
+
 public class Main {
     public static void main(String[] args) {
-        
+        /**
+         * for javafx
+         */
+        NoteGUISystem.run();
     }
 }
